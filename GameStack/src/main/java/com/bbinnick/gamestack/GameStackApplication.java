62b6db4670e77b8f798942670e5485fbf9bb264c
bbinnick <bb539867@southeast.edu>
2024-10-03 21:57:43 -0500

    Real initial commit

---
+++ GameStack/src/main/java/com/bbinnick/gamestack/GameStackApplication.java
@@ -0,0 +1,13 @@
+package com.bbinnick.gamestack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameStackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameStackApplication.class, args);
+	}
+
+}
---
+++ GameStack/src/test/java/com/bbinnick/gamestack/GameStackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bbinnick.gamestack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameStackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
